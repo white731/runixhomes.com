ace9bfb61c97cea6fddc7034bd06c5552ebf2c97
Landon Whitesides <whitesideslandon@gmail.com>
2023-04-21 14:00:18 -0600

    working on fixing problems

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import firebase from 'firebase/compat/app';
+
+var firebaseui = require('firebaseui');
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFmXpDJkOFigewKHjL1ByAqe9UpYxji_0",
+  authDomain: "authentication-example-3107a.firebaseapp.com",
+  projectId: "authentication-example-3107a",
+  storageBucket: "authentication-example-3107a.appspot.com",
+  messagingSenderId: "591515814813",
+  appId: "1:591515814813:web:e537722234d372af10fd74",
+  measurementId: "G-VCPP48ZM6T"
+};
+
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app)
+
+
 function App() {
+
+// Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: false,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+      }
+    ]
+    // Other config options...
+  });
+
+  const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: 'https://www.example.com/finishSignUp?cartId=1234',
+    // This must be true.
+    handleCodeInApp: true,
+    dynamicLinkDomain: 'example.page.link'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
