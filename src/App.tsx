67c8faf14dd71363493533d0d746cdf2529ccd90
Landon Whitesides <whitesideslandon@gmail.com>
2023-04-29 23:55:32 -0600

    is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import GoogleButton from 'react-google-button'
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import firebase from 'firebase/compat/app';
 
-var firebaseui = require('firebaseui');
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBFmXpDJkOFigewKHjL1ByAqe9UpYxji_0",
   authDomain: "authentication-example-3107a.firebaseapp.com",
@@ -25,58 +18,84 @@ const firebaseConfig = {
   measurementId: "G-VCPP48ZM6T"
 };
 
-
-
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
+const provider = new GoogleAuthProvider();
 const auth = getAuth(app)
 
 
+type User = {
+  name: string | null,
+  email: string | null
+} | null
+
+
+// Initialize Firebase
+
 function App() {
 
-// Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  useEffect(()=>{
+    if(localStorage.getItem('email') != null || localStorage.getItem('email') != "null"){
+      setUser({name:localStorage.getItem("name"),email: localStorage.getItem("email")})
+    }
+  })
 
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      {
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        requireDisplayName: false,
-        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+  const [user, setUser] = useState<User>()
+
+  const signIn = () => {
+    signInWithPopup(auth, provider).then(
+      (result)=>{
+        const credential = GoogleAuthProvider.credentialFromResult(result)
+        const token = credential?.accessToken
+        const user = result.user
+        console.log(user)
+        localStorage.setItem("name",user.displayName ? user.displayName : "null")
+        localStorage.setItem("email",user?.email ? user?.email : "null")
+        localStorage.setItem("photoUrl",user?.photoURL ? user?.photoURL : "null")
+        setUser({name:user.displayName, email: user.email})
+  
       }
-    ]
-    // Other config options...
-  });
+    ).catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+      const email = error.customData.email
+      const credential = GoogleAuthProvider.credentialFromError(error)
+    })
+  }
 
-  const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://www.example.com/finishSignUp?cartId=1234',
-    // This must be true.
-    handleCodeInApp: true,
-    dynamicLinkDomain: 'example.page.link'
-  };
+  const logout = () =>{
+    localStorage.clear()
+    setUser(null)
+  }
+
+  const showUser = () => {
+
+    let name = localStorage.getItem('name')
+    let email = localStorage.getItem('email')
+
+    return(
+
+    <div>
+      <h1>{user?.name}</h1>
+      <p>{user?.email}</p>
+    </div>
+    )
+
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+
+      <GoogleButton
+         onClick={signIn}
+      />
+      <button onClick={()=>logout()}>
+        Logout
+      </button>
+      {showUser()}
+    </>
+
   );
 }
 
