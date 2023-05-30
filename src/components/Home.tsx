94f31b644633eeaa248a34cfc0e8e85e881d3145
white731 <whitesideslandon@gmail.com>
2023-05-30 12:35:07 -0600

    added material

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,25 @@
+import {
+  CardContent,
+  Card,
+  Container,
+  Typography,
+  CardActionArea,
+  CardActions,
+} from "@mui/material";
+
 const Home = () => {
-  return <h1>Welcome Home</h1>;
+  return (
+    <Container>
+      <Card title="Welcome to my cool site">
+        <CardContent>
+          <Typography>This is gonna be awesome</Typography>
+        </CardContent>
+        <CardActionArea>
+          <CardActions></CardActions>
+        </CardActionArea>
+      </Card>
+    </Container>
+  );
 };
 
 export default Home;
