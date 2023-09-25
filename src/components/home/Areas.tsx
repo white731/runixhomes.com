a42b3f0a4a2295a7171c1d7f03f187d59027c9fb
white731 <whitesideslandon@gmail.com>
2023-09-25 12:40:32 -0600

    updated home page map thing a bit

--- src/components/home/Areas.tsx
+++ src/components/home/Areas.tsx
@@ -1,27 +1,16 @@
 import { ThemeContext } from "@emotion/react";
-import {
-  Container,
-  Typography,
-  Box
-} from "@mui/material";
+import { Container, Typography, Box, Button } from "@mui/material";
 import { useContext } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import areas from "../../images/areas.png";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Areas = () => {
   const theme = useContext(ThemeContext);
   const navigate = useNavigate();
 
   return (
-    <Container
-      maxWidth="md"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignContent: "center",
-        justifyContent: "center",
-      }}
-    >
     <Box
       sx={{
         display: "flex",
@@ -34,20 +23,108 @@ const Areas = () => {
         justifyContent: "center",
       }}
     >
-      <Typography
-        variant="h4"
-        sx={{
-          marginTop: "40px",
-          textAlign: "center",
-          color: "primary.main",
-        }}
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "Center" }}
       >
-        Areas we Serve
-      </Typography>
-      <img src={areas} alt="areas-we-serve" style={{ height: "600px" }} />
+        <Typography
+          variant="h3"
+          sx={{
+            marginBottom: "20px",
+            textAlign: "center",
+            color: "primary.main",
+          }}
+        >
+          Areas We Serve
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            textAlign: "center",
+            maxWidth: "50%",
+            flexDirection: "column",
+          }}
+        >
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: "center",
+              color: "primary.main",
+            }}
+          >
+            Southern Idaho
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              textAlign: "center",
+              color: "primary.main",
+              marginTop: 1.5,
+              marginBottom: 1.5,
+            }}
+          >
+            Rupert, Burley, Paul, Twin Falls, Declo, Albion, Sun Valley, Hailey,
+            Bellvue, Elba, Malta & Raft River
+          </Typography>
+          <Button
+            onClick={() => window.open("tel:+12082195001")}
+            variant="contained"
+          >
+            <PhoneIcon sx={{ marginRight: "8px" }} />
+            <Typography variant="body1">208 219 5001</Typography>
+          </Button>
+          <Button
+            onClick={() => window.open("mailto:landon@runixhomes.com")}
+            variant="contained"
+            sx={{ marginTop: "7px", marginBottom: 1.5 }}
+          >
+            <EmailIcon sx={{ marginRight: "8px" }} />
+            <Typography variant="body1">landon@runixhomes.com</Typography>
+          </Button>
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: "center",
+              color: "primary.main",
+              marginTop: "15px",
+            }}
+          >
+            Salt Lake City
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              textAlign: "center",
+              color: "primary.main",
+              marginTop: 1.5,
+              marginBottom: 1.5,
+            }}
+          >
+            West Valley, West Jordan, Sandy, Draper, Bluffdale, Orem, Eagle
+            Mountain, Saratoga Springs, Provo, Heber, Park City, & South Salt
+            Lake City
+          </Typography>
+          <Button
+            onClick={() => window.open("tel:+13857438966")}
+            variant="contained"
+          >
+            <PhoneIcon sx={{ marginRight: "8px" }} />
+            <Typography variant="body1">385 743 8966</Typography>
+          </Button>
+          <Button
+            onClick={() => window.open("mailto:bruce@runixhomes.com")}
+            variant="contained"
+            sx={{ marginTop: "7px", marginBottom: 1.5 }}
+          >
+            <EmailIcon sx={{ marginRight: "8px" }} />
+            <Typography variant="body1">bruce@runixhomes.com</Typography>
+          </Button>
+        </Box>
+      </Box>
+      <Box>
+        <img src={areas} alt="areas-we-serve" style={{ height: "600px" }} />
+      </Box>
     </Box>
-    </Container>
-  )
+  );
 };
 
 export default Areas;
